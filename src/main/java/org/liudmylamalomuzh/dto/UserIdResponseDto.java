efc207ba4541a7671901ca59024c3820dd45220d
milafreckled <miladul2014@gmail.com>
2025-02-26 12:13:38 +0100

    Modified MockMVC tests

---
+++ src/main/java/org/liudmylamalomuzh/dto/UserIdResponseDto.java
@@ -0,0 +1,18 @@
+package org.liudmylamalomuzh.dto;
+
+public class UserIdResponseDto {
+    private Integer id;
+
+    public UserIdResponseDto(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
+
